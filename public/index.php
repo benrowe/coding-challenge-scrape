0a46784df41a3f42aa11253c4112d961d077ea10
Ben Rowe <ben.rowe.83@gmail.com>
2025-08-02 11:18:43 +1000

    Build super simple endpoints

--- public/index.php
+++ public/index.php
@@ -5,5 +5,24 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 if ($_SERVER['REQUEST_URI'] === '/article') {
+    // Serve the HTML content for the article
+    // this is because news.com.au blocked scraping
+    // and we are scraping from localhost
+    header('Content-Type: text/html; charset=UTF-8');
     echo file_get_contents(__DIR__.'/../storage/news.html');
+} else if ($_SERVER['REQUEST_URI'] === '/api') {
+
+    // Serve the JSON content for the articles
+
+    header('content-type: application/json');
+    if (!file_exists(__DIR__.'/../storage/articles.json')) {
+
+        echo json_encode(['error' => 'No articles found. Run the scraper first.']);
+        exit;
+    }
+    echo file_get_contents(__DIR__.'/../storage/articles.json');
+} else {
+    // dashboard page
+    header('Content-Type: text/html; charset=UTF-8');
+    echo file_get_contents(__DIR__.'/../resources/views/dashboard.html');
 }
