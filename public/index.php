ff9705e6e8c09383468b118ba92471ab555a465a
Ben Rowe <ben.rowe.83@gmail.com>
2025-08-02 11:06:47 +1000

    Add very simple endpoint to mimic news.com.au

--- public/index.php
+++ public/index.php
@@ -4,3 +4,6 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+if ($_SERVER['REQUEST_URI'] === '/article') {
+    echo file_get_contents(__DIR__.'/../storage/news.html');
+}
